d5f4320bd1741c038edfa995d9ba082dcf31ed4f
manthanmehtapx <manthanmehtapx@gmail.com>
2025-06-14 17:16:37 +0530

    Added Dockerized Selenium Grid and updated test config

--- src/test/java/tests/AmazonSearchTest.java
+++ src/test/java/tests/AmazonSearchTest.java
@@ -1,11 +1,12 @@
-
 package tests;
 
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.By;
 import org.testng.annotations.*;
+
 import java.net.URL;
 
 public class AmazonSearchTest {
@@ -15,9 +16,23 @@ public class AmazonSearchTest {
     @Parameters("browser")
     @BeforeMethod
     public void setup(@Optional("chrome") String browser) throws Exception {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setBrowserName(browser);
-        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+        if (browser.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+        } else {
+            throw new IllegalArgumentException("Browser not supported: " + browser);
+        }
     }
 
     @Test
@@ -26,7 +41,11 @@ public class AmazonSearchTest {
         WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
         searchBox.sendKeys("Bluetooth Earphones");
         searchBox.submit();
-        System.out.println("Title: " + driver.getTitle());
+        System.out.println("Page Title: " + driver.getTitle());
+    }
+
+    public void amazonOptions(){
+
     }
 
     @AfterMethod
